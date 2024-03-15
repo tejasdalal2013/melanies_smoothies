08645c2ed6999e27472780958b39233977f6f966
tejasdalal2013 <66457635+tejasdalal2013@users.noreply.github.com>
2024-03-15 10:41:24 +0530

    Update streamlit_app.py
    Needed and Added Changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,17 +44,17 @@ if ingredients_list:
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + each_fruit)
         fv_dv = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
-    #st.write(ingredients_string)
+        #st.write(ingredients_string)
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+        my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
 
-    #st.write(my_insert_stmt)
+        #st.write(my_insert_stmt)
 
-    time_to_insert = st.button('Submit Orders')
+        time_to_insert = st.button('Submit Orders')
 
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered, ',icon="✅")
+        if time_to_insert:
+            session.sql(my_insert_stmt).collect()
+            st.success('Your Smoothie is ordered, ',icon="✅")
            
 
