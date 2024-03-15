27e62fb9eec8323d099184520b83d2fecdf5a600
tejasdalal2013 <66457635+tejasdalal2013@users.noreply.github.com>
2024-03-15 10:55:50 +0530

    Update streamlit_app.py
    Changes Done

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,10 +51,10 @@ if ingredients_list:
 
         #st.write(my_insert_stmt)
 
-        time_to_insert = st.button('Submit Orders')
+    time_to_insert = st.button('Submit Orders')
 
-        if time_to_insert:
-            session.sql(my_insert_stmt).collect()
-            st.success('Your Smoothie is ordered, ',icon="✅")
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered, ',icon="✅")
            
 
